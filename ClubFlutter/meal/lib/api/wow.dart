67b8e51f15537ba9693c07ffecb1594b7fd23fcb
KJS <digyrh456789@gmail.com>
2022-11-10 23:26:33 +0900

    Chore | API 만지는 즁

--- ClubFlutter/meal/lib/api/wow.dart
+++ ClubFlutter/meal/lib/api/wow.dart
@@ -12,16 +12,16 @@ class test extends StatefulWidget {
 }
 
 class _testState extends State<test> {
-  String firstday = "";
-  String seconday = "";
-  String thirday = "";
-  String fourthday = "";
-  String fifthday = "";
-  String sixthday = "";
-  String sevenday = "";
-  String eightday = "";
-  String nineday = "";
-  String tenday = "";
+  String fday = "";
+  String sday = "";
+  String tday = "";
+  String foday = "";
+  String fiday = "";
+  String sixday = "";
+  String sevday = "";
+  String eday = "";
+  String nday = "";
+  String teday = "";
 
 
   Future<void> fetchPost() async {
@@ -29,19 +29,19 @@ class _testState extends State<test> {
         'http://13.125.225.199:8001/api/school/neisAPI/schedule?year=2022&month=11'));
     var parsingData = jsonDecode(utf8.decode(response.bodyBytes));
     setState(() {
-      firstday = parsingData['Schedule_Day']['1'];
-      seconday = parsingData['Schedule_Day']['2'];
-      thirday = parsingData['Schedule_Day']['3'];
-      fourthday = parsingData['Schedule_Day']['4'];
-      fifthday = parsingData['Schedule_Day']['5'];
-      sixthday = parsingData['Schedule_Day']['6'];
-      sevenday = parsingData['Schedule_Day']['7'];
-      eightday = parsingData['Schedule_Day']['8'];
-      nineday = parsingData['Schedule_Day']['9'];
-      tenday = parsingData['Schedule_Day']['10'];
+      fday = parsingData['Schedule_Day']['1'];
+      sday = parsingData['Schedule_Day']['2'];
+      tday = parsingData['Schedule_Day']['3'];
+      foday = parsingData['Schedule_Day']['4'];
+      fiday = parsingData['Schedule_Day']['5'];
+      sixday = parsingData['Schedule_Day']['6'];
+      sevday = parsingData['Schedule_Day']['7'];
+      eday = parsingData['Schedule_Day']['8'];
+      nday = parsingData['Schedule_Day']['9'];
+      teday = parsingData['Schedule_Day']['10'];
     });
     print(parsingData.statusCode);
-    print(firstday);
+    print(fday);
     if (response.statusCode == 200) {
       print(parsingData);
     } else {
@@ -58,8 +58,13 @@ class _testState extends State<test> {
               onPressed: () {
                 fetchPost();
               },
-              child: Text(firstday)), 
-              Text(firstday, style: const TextStyle(color: Colors.black, fontSize: 25),)
+              child: Text(fday)), 
+              Text(fday, style: const TextStyle(color: Colors.black, fontSize: 25),),
+              Text(sday, style: const TextStyle(color: Colors.black, fontSize: 25),),
+              Text(tday, style: const TextStyle(color: Colors.black, fontSize: 25),),
+              Text(foday, style: const TextStyle(color: Colors.black, fontSize: 25),),
+              Text(fiday, style: const TextStyle(color: Colors.black, fontSize: 25),),
+              Text(sixday, style: const TextStyle(color: Colors.black, fontSize: 25),),
         ],
       ),
     );
