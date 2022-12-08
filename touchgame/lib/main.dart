643c3abbc07dcba3066b56606651975aad93f7d6
KJS <digyrh456789@gmail.com>
2022-12-08 22:57:52 +0900

    ADD : TouchGame 변수 / 클릭 이벤트

--- touchgame/lib/main.dart
+++ touchgame/lib/main.dart
@@ -20,30 +20,25 @@ class Touchgame extends StatelessWidget {
 
 class Mainpages extends StatefulWidget {
   const Mainpages({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
   State<Mainpages> createState() => _MainpagesState();
 }
 
 class _MainpagesState extends State<Mainpages> {
+  int counter = 100000;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
+      body: Column(
+        children: [
+          Text(
+            '$counter'
+          ),
+        ]
       ), 
     );
   }
